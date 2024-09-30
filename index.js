c3eebb6a1e575d53ca80fea492f0ccef0c961f3e
mariely <lorenzomariely@gmail.com>
2024-09-30 09:56:01 -0400

    deleted and restarted

--- index.js
+++ index.js
@@ -1,58 +1,26 @@
-var options = document.querySelectorAll('.choice')
-var  value = choice.textContent;
-var computerChoice = choice[Math.floor(Math.random() * 3)];
-var rock = 1
-var paper = 2
-var scissors = 3
-value = [rock, paper, scissors]
+var choice = document.querySelectorAll('.choice');
+var resultDiv = document.querySelector('.result');
+var choices = ['Rock', 'Paper', 'Scissors'];
+let computer = document.getElementById("#computerchoice")
+computer = choices[Math.floor(Math.random() * 3)]
+result = winner(computer, you)
 
 
-function rock(){
-    if (computerChoice === 1 && value[0] ){
-    
-       x =  document.createElement("h1")
-        h1.textContent = "TIE!"
-    }
-    else if (computerChoice === 2 && value[0]){
-    x= document.createElement("h1")
-     h1.textContent = "YOU LOST:("
-    }
-    else if (computerChoice === 3 && value[0]){
-       x =  document.createElement("h1")
-        h1.textContent = "YOU WON!"
-    }
-    return x
-
-}
-
-function paper(){
-    if (computerChoice === 1 && value[1] ){
-         document.createElement("h1")
-        h1.textContent = "YOU WON!"
-    }
-    else if (computerChoice === 2 && value[1]){
-        document.createElement("h1")
-        h1.textContent = "TIE!"
-    }
-    else if (computerChoice === 3 && value[1]){
-       document.createElement("h1")
-        h1.textContent = "YOU LOST:("
-    }
-}
-
-function scissors(){
-    if (computerChoice === 1 && value[2] ){
-        document.createElement("h1")
-        h1.textContent = "YOU LOST:("
-    }
-    else if (computerChoice === 2 && value[2]){
-         document.createElement("h1")
-        h1.textContent = "YOU WON!"
-    }
-    else if (computerChoice === 3 && value[2]){
-        document.createElement("h1")
-        h1.textContent = "TIE!"
-    }
-}
 
 
+
+
+
+function winner(computer, you){
+        if (player === computer) {
+            return "TIE!";
+        } else if (
+            (player === 'Rock' && computer === 'Scissors') ||
+            (player === 'Paper' && computer === 'Rock') ||
+            (player === 'Scissors' && computer === 'Paper')
+        ) {
+            return 'YOU WIN!';
+        } else {
+            return 'YOU LOST:(';
+        }
+    }
