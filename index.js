7a692e89760acbc1329fe0f471a4a64b720c4c47
mariely <lorenzomariely@gmail.com>
2024-09-30 11:35:51 -0400

    added time and computer choice

--- index.js
+++ index.js
@@ -2,26 +2,38 @@ var choice = document.querySelectorAll('.choice');
 var resultDiv = document.querySelector('.result');
 var choices = ['Rock', 'Paper', 'Scissors'];
  var randomComputerChoice = choices[Math.floor(Math.random() * 3)];
-result = winner(computer, you);
+// result = winner(computer, option);
 
 
 
 
 function userChoice(option){
+    document.getElementById("computerchoice").innerHTML = ""; 
     // alert(option);
+    setTimeout(() => {
+        console.log("computers turn");
+        }, 2000);
     let computer = computerChoice()
     winner(option, computer)
 }
 
 function computerChoice(){
     
-    alert(randomComputerChoice);
-    document.getElementById("#computerchoice").innerHTML = randomComputerChoice;
+    // alert(randomComputerChoice);
+   
+    setTimeout(() => {
+        console.log("computers turn");
+        }, 2000);
+        document.getElementById("computerchoice").innerHTML = randomComputerChoice;  
+    // alert( document.getElementById("computerchoice").innerHTML);
     return randomComputerChoice ;
 
 }
 
 function winner(option, computer){
+    setTimeout(() => {
+        console.log("computers turn");
+        }, 2000);
         if (option === computer) {
             alert("TIE!");
         }
@@ -35,4 +47,7 @@ function winner(option, computer){
         else {
             alert("NOT A TIE");
         }
+       
+       
     }
+   
