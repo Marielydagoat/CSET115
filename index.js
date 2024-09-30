9382da479e26a037569bd6a49bac57a3840153c6
mariely <lorenzomariely@gmail.com>
2024-09-30 10:46:21 -0400

    added less functions and user input

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 var choice = document.querySelectorAll('.choice');
 var resultDiv = document.querySelector('.result');
 var choices = ['Rock', 'Paper', 'Scissors'];
-let computer = document.getElementById("#computerchoice")
-computer = choices[Math.floor(Math.random() * 3)]
-result = winner(computer, you)
+ var randomComputerChoice = choices[Math.floor(Math.random() * 3)];
+result = winner(computer, you);
 
 
 
 
+function userChoice(option){
+    // alert(option);
+    let computer = computerChoice()
+    winner(option, computer)
+}
 
+function computerChoice(){
+    
+    alert(randomComputerChoice);
+    document.getElementById("#computerchoice").innerHTML = randomComputerChoice;
+    return randomComputerChoice ;
 
+}
 
-function winner(computer, you){
-        if (player === computer) {
-            return "TIE!";
-        } else if (
-            (player === 'Rock' && computer === 'Scissors') ||
-            (player === 'Paper' && computer === 'Rock') ||
-            (player === 'Scissors' && computer === 'Paper')
-        ) {
-            return 'YOU WIN!';
-        } else {
-            return 'YOU LOST:(';
+function winner(option, computer){
+        if (option === computer) {
+            alert("TIE!");
+        }
+        //  else if (
+        //     (player === 'Rock' && computer === 'Scissors') ||
+        //     (player === 'Paper' && computer === 'Rock') ||
+        //     (player === 'Scissors' && computer === 'Paper')
+        // ) {
+        //     return 'YOU WIN!';
+        // } 
+        else {
+            alert("NOT A TIE");
         }
     }
