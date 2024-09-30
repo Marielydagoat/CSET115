1297f55ee4e681c4a979b1dc53236e518f751dda
mariely <lorenzomariely@gmail.com>
2024-09-30 19:44:53 -0400

    before update

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ var resultDiv = document.querySelector('.result');
 var choices = ['Rock', 'Paper', 'Scissors'];
  var randomComputerChoice = choices[Math.floor(Math.random() * 3)];
 // result = winner(computer, option);
-
+var score = 0
 
 
 
 function userChoice(option){
     document.getElementById("computerchoice").innerHTML = ""; 
     // alert(option);
-    setTimeout(() => {
-        console.log("computers turn");
-        }, 2000);
+
     let computer = computerChoice()
     winner(option, computer)
 }
@@ -21,29 +19,26 @@ function computerChoice(){
     
     // alert(randomComputerChoice);
    
-    setTimeout(() => {
-        console.log("computers turn");
-        }, 2000);
+
         document.getElementById("computerchoice").innerHTML = randomComputerChoice;  
     // alert( document.getElementById("computerchoice").innerHTML);
+
     return randomComputerChoice ;
 
 }
 
 function winner(option, computer){
-    setTimeout(() => {
-        console.log("computers turn");
-        }, 2000);
+    let score = 0
         if (option === computer) {
             alert("TIE!");
         }
-        //  else if (
-        //     (player === 'Rock' && computer === 'Scissors') ||
-        //     (player === 'Paper' && computer === 'Rock') ||
-        //     (player === 'Scissors' && computer === 'Paper')
-        // ) {
-        //     return 'YOU WIN!';
-        // } 
+         else if (
+            (player === 'Rock' && computer === 'Scissors') ||
+            (player === 'Paper' && computer === 'Rock') ||
+            (player === 'Scissors' && computer === 'Paper')
+        ) { score = score + 1
+            return  score && 'YOU WIN!' ;
+        } 
         else {
             alert("NOT A TIE");
         }
