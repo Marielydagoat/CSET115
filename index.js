6e109e059d207badbff5a8a1b1d989b94c4ada0e
mariely <lorenzomariely@gmail.com>
2024-09-30 21:19:44 -0400

    fixed the function, added css, fixed html

--- index.js
+++ index.js
@@ -1,48 +1,28 @@
-var choice = document.querySelectorAll('.choice');
-var resultDiv = document.querySelector('.result');
-var choices = ['Rock', 'Paper', 'Scissors'];
- var randomComputerChoice = choices[Math.floor(Math.random() * 3)];
-// result = winner(computer, option);
-var score = 0
 
+let playerScore = 0;
+let computerScore = 0;
 
-
-function userChoice(option){
-    document.getElementById("computerchoice").innerHTML = ""; 
-    // alert(option);
-
-    let computer = computerChoice()
-    winner(option, computer)
-}
-
-function computerChoice(){
+function userChoice(playerChoice){
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+  let result = '';
+  
+  if (playerChoice === computerChoice){
+    result = `Its a tie! You both chose ${playerChoice}.`;
+  } else if ( (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
+            (playerChoice === 'Paper' && computerChoice === 'Rock') ||
+            (playerChoice === 'Scissors' && computerChoice === 'Paper')
+    ){
+     result = `You win! ${playerChoice} computer chose ${computerChoice}.`;
+    playerScore++;
+  } else {
+    result = `You lose! ${computerChoice} computer chose ${playerChoice}.`;
+    computerScore++;
+  }
+  
+     document.getElementById('result').innerText = result;
+     document.getElementById('score').innerText = `Player: ${playerScore} | Computer: ${computerScore}`;
     
-    // alert(randomComputerChoice);
-   
-
-        document.getElementById("computerchoice").innerHTML = randomComputerChoice;  
-    // alert( document.getElementById("computerchoice").innerHTML);
-
-    return randomComputerChoice ;
-
 }
-
-function winner(option, computer){
-    let score = 0
-        if (option === computer) {
-            alert("TIE!");
-        }
-         else if (
-            (player === 'Rock' && computer === 'Scissors') ||
-            (player === 'Paper' && computer === 'Rock') ||
-            (player === 'Scissors' && computer === 'Paper')
-        ) { score = score + 1
-            return  score && 'YOU WIN!' ;
-        } 
-        else {
-            alert("NOT A TIE");
-        }
-       
-       
-    }
    
